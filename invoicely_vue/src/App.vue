9e689423b2c67a1ff23c4d14690fb798770d118f
kik369 <kris@refaktor.dev>
2021-12-30 20:32:00 +0000

    add vue

---
+++ invoicely_vue/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"
+          ><strong>Invoicely</strong></router-link
+        >
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/dashboard" class="navbar-item"
+              >Dashboard</router-link
+            >
+            ></template
+          >
+          <template v-else>
+            <router-link to="/" class="navbar-item">Home</router-link>
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/sign-up" class="button is-success"
+                  ><strong>Sign Up</strong></router-link
+                >
+                <router-link to="/log-in" class="button is-light"
+                  >Log In</router-link
+                >
+              </div>
+            </div>
+          </template>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view />
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  beforeCreated() {
+    this.$store.commit("initializeStore");
+    const token = this.$store.state.token;
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Token " + token;
+    } else {
+      xios.defaults.headers.common["Authorization"] = "";
+    }
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
---
+++ invoicely_vue/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
